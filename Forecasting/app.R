e1c9337ca8b717f873893cbaf08e48cf929ef88a
hanggaa <hanggaprihadi@gmail.com>
2021-05-29 12:32:11 +0700

    Nambah Catatan

--- Forecasting/app.R
+++ Forecasting/app.R
@@ -2,41 +2,61 @@
 library(shinydashboard)
 
 ui <- dashboardPage(
-    header <- dashboardHeader(title = "Forecasting"),
+    header <- dashboardHeader(title = "O.S Forecasting"),
     
     ## Sidebar content
     sidebar <- dashboardSidebar(
         sidebarMenu(
-            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-            menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+            menuItem("Rice Production", tabName = "dashboard1", icon = icon("dashboard")),
+            menuItem("Rice Price", tabName = "dashboard2", icon = icon("dashboard")),
+            menuItem("Controller", tabName = "controller", icon = icon("th"))
         )
     ),
     ## Body content
     body <- dashboardBody(
         tabItems(
-            # First tab content
-            tabItem(tabName = "dashboard",
+            # First Tab Content
+            tabItem(tabName = "dashboard1",
                     fluidRow(
-                        box(title = "Forecast Rice Price", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 215),
+                        box(title = "Forecast Rice Produce Holt-Winter Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 200),
                             verbatimTextOutput("detail")
                             ),
-                        box(title = "Forecast Rice Produce", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 215),
+                        box(title = "Forecast Rice Produce MLP Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 200),
                             verbatimTextOutput("detail2")
                             ),
-                        box(
-                            title = "Controls", status = "danger", solidHeader = TRUE, collapsible = TRUE, 
-                            sliderInput("slider", "Number of observations:", 1, 100, 50),
-                            textInput("text", "Text Input:")
-                        ),
-                        box(
-                            title = "Blabla", status = "warning", solidHeader = TRUE, collapsible = TRUE
+                        box(title = "Forecast Rice Produce Auto-Arima Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot3", height = 200),
+                            verbatimTextOutput("detail3")
                         )
                     )
             ),
-            
-            # Second tab content
-            tabItem(tabName = "widgets",
-                    h2("Widgets tab content")
+            # Second Tab Content
+            tabItem(tabName = "dashboard2",
+                    fluidRow(
+                       box(title = "Forecast Rice Price Holt-Winter Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot4", height = 200),
+                           verbatimTextOutput("detail4")
+                           ),
+                       box(title = "Forecast Rice Price MLP Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot5", height = 200),
+                           verbatimTextOutput("detail5")
+                       ),
+                       box(title = "Forecast Rice Price Auto-Arima Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot6", height = 200),
+                           verbatimTextOutput("detail6")
+                       )
+                    )
+            ),
+            # Third tab content
+            tabItem(tabName = "controller",
+                    fluidRow(
+                        box(title = "Choose Regional Zone of Rice", status = "danger", solidHeader = TRUE, collapsible = TRUE,
+                            selectInput("reg",
+                                        label = "Choose Regional",
+                                        choices = c("West Java",
+                                                    "Central Java",
+                                                    "East Java",
+                                                    "South Sulawesi"),
+                                        selected = "East Java")
+                        )
+                        
+                    )
             )
         )
     ),
@@ -48,30 +68,74 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+    #Rice Produce
     hangga = read.csv("D:/supply_beras.csv",sep = ",")
-    head(hangga)
     cb <- hangga$Karawang
     supply <- ts(cb, start (2012,1), frequency = 12)
     plot(supply)
     library(forecast)
     library(nnfor)
-    #HW
+    #HW Produce
     hw <- HoltWinters(supply)
     plot(hw)
-    f1 <- forecast(hw, h=90)
-    detail2 <- accuracy(f1)
-    #MLP
+    fhwp <- forecast(hw, h=90)
+    detail <- accuracy(fhwp)
+    #MLP Produce
     ml <- mlp(supply)
     plot(ml)
-    f2 = forecast(ml, h=90)
-    detail <- accuracy(f2)
+    fmlpp = forecast(ml, h=90)
+    detail2 <- accuracy(fmlpp)
+    #ARIMA Produce
+    ar <- auto.arima(supply)
+    farp <- forecast(ar, h = 140)
+    plot(farp)
+    detail3 <- accuracy(f3)
+    
+    #Rice Price
+    hangga2 = read.csv("D:/harga_beras.csv", sep = ";")
+    cb2 <- hangga2$Jatim
+    supply2 <- ts(cb2, start (2017,1), frequency = 12)
+    plot(supply2)
+    #HW Produce
+    hw2 <- HoltWinters(supply2)
+    plot(hw2)
+    fhwp2 <- forecast(hw2, h=140)
+    plot(fhwp2)
+    detail4 <- accuracy(fhwp2)
+    #MLP Produce
+    ml2 <- mlp(supply2)
+    plot(ml2)
+    fmlpp2 <- forecast(ml2, h=140)
+    plot(fmlpp2)
+    detail5 <- accuracy(fmlpp2)
+    #ARIMA Produce
+    ar2 <- auto.arima(supply2)
+    farp2 <- forecast(ar2, h = 140)
+    plot(farp2)
+    detail6 <- accuracy(farp2)
     output$detail <- renderText({ detail[,"MAPE"] })
     output$detail2 <- renderText({ detail2[,"MAPE"]})
+    output$detail3 <- renderText({ detail3[,"MAPE"]})
+    output$detail4 <- renderText({ detail4[,"MAPE"] })
+    output$detail5 <- renderText({ detail5[,"MAPE"]})
+    output$detail6 <- renderText({ detail6[,"MAPE"]})
     output$plot1 <- renderPlot({
-        plot(f1)
+        plot(fhwp)
     })
     output$plot2 <- renderPlot({
-        plot(f2)
+        plot(fmlpp)
+    })
+    output$plot3 <- renderPlot({
+        plot(farp)
+    })
+    output$plot4 <- renderPlot({
+        plot(fhwp2)
+    })
+    output$plot5 <- renderPlot({
+        plot(fmlpp2)
+    })
+    output$plot6 <- renderPlot({
+        plot(farp2)
     })
 }
 
